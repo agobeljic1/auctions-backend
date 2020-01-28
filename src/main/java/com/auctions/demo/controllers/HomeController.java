87f5aa59747e3194e01a1d7d593eb5ec8331fb30
unknown <agobeljic1@hotmail.com>
2020-01-28 13:52:02 +0100

    Initial commit

---
+++ src/main/java/com/auctions/demo/controllers/HomeController.java
@@ -0,0 +1,4 @@
+package com.auctions.demo.controllers;
+
+public class HomeController {
+}
---
+++ src/test/java/com/auctions/demo/AuctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auctions.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
